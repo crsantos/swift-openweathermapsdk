966c570afc8c8f0aa19a15f1fbffd9a0b68670f5
Carlos Santos <carlos.santos@blip.pt>
2014-09-27 16:31:59 +0100

    Initial Commit

---
+++ OpenWeatherMapSDK/OpenWeatherMapSDK.h
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherMapSDK.h
+//  OpenWeatherMapSDK
+//
+//  Created by Carlos Santos on 27/09/14.
+//  Copyright (c) 2014 Carlos Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWeatherMapSDK.
+FOUNDATION_EXPORT double OpenWeatherMapSDKVersionNumber;
+
+//! Project version string for OpenWeatherMapSDK.
+FOUNDATION_EXPORT const unsigned char OpenWeatherMapSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherMapSDK/PublicHeader.h>
+
+
